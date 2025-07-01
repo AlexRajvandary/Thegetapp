fb183bd006d4f390a8fac561faacf261cf70b65e
Alex <stark.11raj@gmail.com>
2025-07-01 17:08:15 +0300

    full screen

--- thegetapp/src/main.tsx
+++ thegetapp/src/main.tsx
@@ -8,7 +8,7 @@ import RouterWrapper from './RouterWrapper.jsx';
 import './index.css'
 import { AuthProvider } from './components/AuthContext.js';
 import { init, backButton } from '@telegram-apps/sdk-react';
-import { closingBehavior, swipeBehavior } from '@telegram-apps/sdk';
+import { closingBehavior, swipeBehavior, requestFullscreen } from '@telegram-apps/sdk';
 
 
 
@@ -23,6 +23,10 @@ if (closingBehavior.mount.isAvailable()) {
 
 if (swipeBehavior.mount.isAvailable()) {
   swipeBehavior.mount();
+}
+
+if (requestFullscreen.isAvailable()) {
+  await requestFullscreen();
 }
 
 createRoot(document.getElementById('root')!).render(
